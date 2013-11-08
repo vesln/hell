d8f72ba4d9d419693fb685ea79fe324e02ac54c7
Veselin Todorov <veselin@veselin.bg>
2013-11-08 12:22:12 +0200

    Implement function signatures

---
+++ test/fixtures/signature.js
@@ -0,0 +1,24 @@
+function named() {}
+
+(function() {})();
+
+var foo = function() {};
+
+foo = function() {};
+
+foo.bar = function() {};
+
+foo.bar.baz = function() {};
+
+foo.bar.baz.boo = function() {};
+
+foo.bar.baz.boo.moo = function() {};
+
+foo['bar'] = function() {}
+foo['bar']['baz'] = function() {}
+foo['bar']['baz']['boo'] = function() {}
+
+foo[1]['baz'] = function() {}
+
+foo['baz'].bar = function() {}
+foo[index()].bar = function() {}
---
+++ test/signatures.test.js
@@ -0,0 +1,34 @@
+describe('hell', function() {
+  var source = null;
+
+  beforeEach(function() {
+    source = hell.analyzeFile(fixture('signature.js'));
+  });
+
+  it('has a signature for Program', function() {
+    source.contexts[0].signature().should.eq('main');
+  });
+
+  it('has a signature for named function', function() {
+    source.contexts[1].signature().should.eq('named');
+  });
+
+  it('has a signature for anon functions', function() {
+    source.contexts[2].signature().should.eq('anon');
+  });
+
+  it('has a signature for assigned functions', function() {
+    source.contexts[3].signature().should.eq('foo');
+    source.contexts[4].signature().should.eq('foo');
+    source.contexts[5].signature().should.eq('foo.bar');
+    source.contexts[6].signature().should.eq('foo.bar.baz');
+    source.contexts[7].signature().should.eq('foo.bar.baz.boo');
+    source.contexts[8].signature().should.eq('foo.bar.baz.boo.moo');
+    source.contexts[9].signature().should.eq('foo.bar');
+    source.contexts[10].signature().should.eq('foo.bar.baz');
+    source.contexts[11].signature().should.eq('foo.bar.baz.boo');
+    source.contexts[12].signature().should.eq('foo.1.baz');
+    source.contexts[13].signature().should.eq('foo.baz.bar');
+    source.contexts[14].signature().should.eq('foo.?.bar');
+  });
+});
